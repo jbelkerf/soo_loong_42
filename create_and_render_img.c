c824b2fbbb9f2ab6462f900c7169902fa2ea9dcd
jbelkerf <jbelkerf@gmail.com>
2025-02-22 17:50:05 +0100

    ckeck the file if exist and in the correct format

--- create_and_render_img.c
+++ create_and_render_img.c
@@ -6,7 +6,7 @@
 /*   By: jbelkerf <jbelkerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:52:12 by jbelkerf          #+#    #+#             */
-/*   Updated: 2025/02/22 16:54:22 by jbelkerf         ###   ########.fr       */
+/*   Updated: 2025/02/22 17:48:08 by jbelkerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ mlx_image_t	*create_render(mlx_t *mlx, char *img_file, int symbol, char **map)
 
 	img_tex = mlx_load_png(img_file);
 	img = mlx_texture_to_image(mlx, img_tex);
+	mlx_delete_texture(img_tex);
 	mlx_resize_image(img, 50, 50);
 	render_img(map, mlx, img, symbol);
 	return (img);
--- main.c
+++ main.c
@@ -6,16 +6,48 @@
 /*   By: jbelkerf <jbelkerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:22:58 by jbelkerf          #+#    #+#             */
-/*   Updated: 2025/02/22 17:17:39 by jbelkerf         ###   ########.fr       */
+/*   Updated: 2025/02/22 17:39:52 by jbelkerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	check_the_file_extention(char *file)
+{
+	int	name_lenght;
+
+	name_lenght = ft_strlen(file);
+	if (name_lenght <= 4)
+		puts_error("file name not correct");
+	if (file[name_lenght - 1] != 'r' || file[name_lenght - 2] != 'e')
+		puts_error("file name not correct");
+	if (file[name_lenght - 3] != 'b' || file[name_lenght - 4] != '.')
+		puts_error("file name not correct");
+	if (file[name_lenght - 5] == '/')
+		puts_error("file name not correct");
+}
+
+void	check_the_file_readablity(char *file)
+{
+	int	fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		puts_error("file not exist or not readable");
+	close (fd);
+}
+
+void	check_the_file(char *file)
+{
+	check_the_file_extention(file);
+	check_the_file_readablity(file);
+}
+
 void	is_the_map_valid(char *file)
 {
 	char	**map;
 
+	check_the_file(file);
 	map = map_to_str(file);
 	check_non_wanted_symbol(map);
 	check_the_walls(map);
